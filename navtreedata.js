d01179df378b234f05750dddf932ccc11cab0928
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-06 00:53:54 +0000

    Deploy ImpulseAdventure/GUIslice to github.com/ImpulseAdventure/GUIslice.git:gh-pages

---
+++ navtreedata.js
@@ -0,0 +1,46 @@
+var NAVTREE =
+[
+  [ "GUIslice", "index.html", [
+    [ "GUIslice library", "index.html", null ],
+    [ "Todo List", "todo.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"GUIslice_8c.html",
+"GUIslice__drv__m5stack_8h.html#a1302e252eef8c80542aa9157521555ef",
+"XGauge_8c.html#a873732a73c878d2883f280fcaa199f71",
+"XRadial_8h.html#a9c1e2dd972efce61e54ccfe9b5026627",
+"functions_vars_g.html",
+"group____GraphicsGenFunc__.html#ga277a826920bf21dac264788c9a0480a0",
+"group____IntPage__.html#gae0468b3561425038114e97ac70b6a04a",
+"group____Int__.html#ga9efed32d0bce39b8cd7c66d1d7a34308ae1d511480a05b47db3cc539056227494",
+"group____MacroFlash__.html#ga0c98c237135782d2f54e905319122164",
+"structgslc__tsXRingGauge.html#a97b5769e16f2c66d4e217a319894c1bb"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
